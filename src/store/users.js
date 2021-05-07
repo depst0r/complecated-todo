f0f1099216ce3580f9898e3b3e956b311e079be5
depst0r <depstor90@gmail.com>
2021-05-07 17:39:40 +0300

    users

--- src/store/users.js
+++ src/store/users.js
@@ -1,5 +1,10 @@
 import { types } from 'mobx-state-tree'
 
-const UsersStore = types.modal('UsersStore', {})
+const UsersStore = types.modal('UsersStore', {
+  id: types.identifierNu,
+  createdAt: types.string,
+  name: types.string,
+  avatar: types.string,
+})
 
 export default UsersStore
